af10df8e5de3009379d265344253e9b473287cc4
sejalg1096 <sejalg1096@gmail.com>
2020-04-24 13:59:53 +0530

    First Commit : Create the directory structure and set up express server

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const port = 100;
+
+app.listen(port , function(error){
+    if(error){
+        console.log(`Error in running the server :${error}`);
+        return;
+    }
+
+    console.log(`Server is up and running on port : ${port}`);
+});
